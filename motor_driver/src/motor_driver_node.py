7a5937146bf3c8dbba83b73bbade94ed9408cdfa
Denis Shustov <denis.shustov.1984@gmail.com>
2020-09-28 18:47:45 +0300

    Add files via upload

--- motor_driver/src/motor_driver_node.py
+++ motor_driver/src/motor_driver_node.py
@@ -16,38 +16,50 @@ class Motor_Driver:
     SPR = 200   # Steps per Revolution (360 / 1.8)
     delay_const = .00243
 
-    RESOLUTION = {'Full': (0, 0, 0),
-                  'Half': (1, 0, 0),
-                  '1/4': (0, 1, 0),
-                  '1/8': (1, 1, 0),
-                  '1/16': (0, 0, 1),
-                  '1/32': (1, 0, 1)}
+    # RESOLUTION = {'Full': (0, 0, 0),
+    #               'Half': (1, 0, 0),
+    #               '1/4': (0, 1, 0),
+    #               '1/8': (1, 1, 0),
+    #               '1/16': (0, 0, 1),
+    #               '1/32': (1, 0, 1)}
 
-    def __init__(self, dir1, step1, mode1, dir2, step2, mode2):
+# mode1 mode2
+    def __init__(self, dir1, step1, en1, dir2, step2, en2):
         self.delay = self.delay_const
         self.DIR1 = dir1
         self.STEP1 = step1
         self.DIR2 = dir2
         self.STEP2 = step2
+        self.EN1 = en1
+        self.EN2 = en2
+        self.prev_rpm_left = 0
+        self.prev_rpm_right = 0
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.DIR1, GPIO.OUT)
         GPIO.setup(self.STEP1, GPIO.OUT)
 
+        GPIO.setup(self.EN1, GPIO.OUT)
+        GPIO.setup(self.EN2, GPIO.OUT)
+
+        GPIO.output(self.EN1, GPIO.HIGH)
+        GPIO.output(self.EN2, GPIO.HIGH)
+        rospy.loginfo("Disabled drivers, speed 0!")
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.DIR2, GPIO.OUT)
         GPIO.setup(self.STEP2, GPIO.OUT)
 
-        # (14, 15, 18)   # Microstep Resolution GPIO Pins
-        self.MODE1 = mode1
-        GPIO.setup(self.MODE1, GPIO.OUT)
+        # # (14, 15, 18)   # Microstep Resolution GPIO Pins
+        # self.MODE1 = mode1
+        # GPIO.setup(self.MODE1, GPIO.OUT)
 
-        # (14, 15, 18)   # Microstep Resolution GPIO Pins
-        self.MODE2 = mode2
-        GPIO.setup(self.MODE2, GPIO.OUT)
+        # # (14, 15, 18)   # Microstep Resolution GPIO Pins
+        # self.MODE2 = mode2
+        # GPIO.setup(self.MODE2, GPIO.OUT)
 
-        GPIO.output(self.MODE1, self.RESOLUTION['Full'])
-        GPIO.output(self.MODE2, self.RESOLUTION['Full'])
+        # GPIO.output(self.MODE1, self.RESOLUTION['Full'])
+        # GPIO.output(self.MODE2, self.RESOLUTION['Full'])
 
     def __rpm_to_delay(self, rpm):
         if rpm == 0:
@@ -62,37 +74,50 @@ class Motor_Driver:
             delay = self.delay_const*(60/rpm)
         return delay
 
-    def run(self, rpm1, rpm2):  # first motor, second motor
+    def run(self, rpm_left, rpm_right):  # first motor, second motor
 
-        #print('rpm1'+str(rpm1))
-        #print('rpm2'+str(rpm2))
+        # print('rpm_left'+str(rpm_left))
+        # print('rpm_right'+str(rpm_right))
 
-        if rpm1 == 0 and rpm2 == 0:
+        if rpm_left == 0 and rpm_right == 0:
+            if(self.prev_rpm_left != rpm_left and self.prev_rpm_right != rpm_right):
+                rospy.loginfo("Disabled drivers, speed 0!")
+                GPIO.output(self.EN1, GPIO.HIGH)
+                GPIO.output(self.EN2, GPIO.HIGH)
+                self.prev_rpm_left = rpm_left
+                self.prev_rpm_right = rpm_right
             return
+        else:
+            if(self.prev_rpm_left != rpm_left and self.prev_rpm_right != rpm_right):
+                rospy.loginfo("Enabled drivers, rpm "+str(rpm_left))
+                GPIO.output(self.EN1, GPIO.LOW)
+                GPIO.output(self.EN2, GPIO.LOW)
+
+        self.prev_rpm_left = rpm_left
+        self.prev_rpm_right = rpm_right
 
         start = time.time()
 
-        GPIO.output(self.DIR1, rpm1 > 0 if self.CW else self.CCW)
-        GPIO.output(self.DIR2, rpm2 > 0 if self.CW else self.CCW)
+        GPIO.output(self.DIR1, rpm_left > 0 if self.CW else self.CCW)
+        GPIO.output(self.DIR2, rpm_right > 0 if self.CW else self.CCW)
 
-        rpm1 = abs(rpm1)
-        rpm2 = abs(rpm2)
+        rpm_left = abs(rpm_left)
+        rpm_right = abs(rpm_right)
 
-        self.delay = self.__rpm_to_delay(rpm1)
+        self.delay = self.__rpm_to_delay(rpm_left)
 
-        #for x in range(self.SPR):
         GPIO.output(self.STEP1, GPIO.HIGH)
         GPIO.output(self.STEP2, GPIO.HIGH)
- 
+
         sleep(self.delay)
- 
+
         GPIO.output(self.STEP1, GPIO.LOW)
         GPIO.output(self.STEP2, GPIO.LOW)
 
         sleep(self.delay)
 
         end = time.time()
-        print(end-start)
+        # print(end-start)
 
 
 class Driver:
@@ -107,14 +132,14 @@ class Driver:
         angular = msg.angular.z
 
         # Calculate wheel speeds in m/s
-        self._left_speed = linear - angular
-        self._right_speed = linear + angular
+        self._left_speed = ((angular*0.29)/2)+linear
+        self._right_speed = ((linear*2) - self._left_speed)
 
     def __init__(self):
         rospy.init_node('driver')
 
         self.motor_driver = Motor_Driver(
-            20, 21, (14, 15, 18), 26, 19, (6, 5, 13))
+            16, 20, 6,    26, 19, 21)
 
         self._left_speed = 0
         self._right_speed = 0
@@ -125,7 +150,7 @@ class Driver:
 
         self.ros_sub_twist = rospy.Subscriber("/cmd_vel", Twist, self.callback)
         rospy.loginfo("Initialization complete")
-        #rospy.spin()
+        # rospy.spin()
 
     def run(self):
 
@@ -154,6 +179,8 @@ def main():
     except:
         print("Unexpected error:", sys.exc_info()[0])
     finally:
+        GPIO.output(16, GPIO.HIGH)
+        GPIO.output(12, GPIO.HIGH)
         GPIO.cleanup()
 
 
