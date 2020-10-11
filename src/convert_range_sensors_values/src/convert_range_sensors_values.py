b4f3b25c27c39f80665568dd74f7178d2ad74973
denisshustov <denis.shustov.1984@gmail.com>
2020-10-11 20:44:00 +0300

    fix range sensors & change usb port

--- src/convert_range_sensors_values/src/convert_range_sensors_values.py
+++ src/convert_range_sensors_values/src/convert_range_sensors_values.py
@@ -5,37 +5,36 @@ from sensor_msgs.msg import Range
 from std_msgs.msg import Float32MultiArray
 import rospy
 
-pub_range1 = rospy.Publisher('/ppp/range1', Range, queue_size=10)
-pub_range2 = rospy.Publisher('/ppp/range2', Range, queue_size=10)
-pub_range3 = rospy.Publisher('/ppp/range3', Range, queue_size=10)
+pub_range1 = rospy.Publisher('/ppp/range_sensor1', Range, queue_size=10)
+pub_range2 = rospy.Publisher('/ppp/range_sensor2', Range, queue_size=10)
+pub_range3 = rospy.Publisher('/ppp/range_sensor3', Range, queue_size=10)
 
-def publishRange(range_pub, data, frame_id):
-    range_pub = Range()
-    range_pub.header.stamp = rospy.Time.now()
-    range_pub.header.frame_id = frame_id
-    range_pub.radiation_type = 0
-    range_pub.field_of_view = 0.34 #horizontal ~20, vertical ~13 -> radians 0.34
-    range_pub.min_range = 0.02
-    range_pub.max_range = 0.6
-    range_pub.range = data
+def publishRange(range_publisher, data, frame_id):
+    range_msg = Range()
+    range_msg.header.stamp = rospy.Time.now()
+    range_msg.header.frame_id = frame_id
+    range_msg.radiation_type = 1
+    range_msg.field_of_view = 0.34 #horizontal ~20, vertical ~13 -> radians 0.34
+    range_msg.min_range = 0.02
+    range_msg.max_range = 0.6
+    range_msg.range = data/100.0
+    range_publisher.publish(range_msg)
+
 
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard {}-{}-{}".format(data.data[0],data.data[1],data.data[2]))
-
-    publishRange(pub_range1,data.data[0],"/sonyc_1")
-    publishRange(pub_range1,data.data[1],"/sonyc_Mirror__1")
-    publishRange(pub_range1,data.data[2],"/sonyc__1__1")
-    
-    pub.publish(r)
-    rospy.sleep(1.0)
+    publishRange(pub_range1,data.data[1],"sonyc_1") #right 
+    publishRange(pub_range2,data.data[0],"sonyc_Mirror__1") # left
+    publishRange(pub_range3,data.data[2],"sonyc__1__1") #forward
 
 def main():
+    global pub_range
     rospy.init_node('convert_range_sensors_values', anonymous=True)
     rospy.loginfo("convert_range_sensors_values initialization ok!")
     
-    rospy.Subscriber("rangeSonar1", Float32MultiArray, callback)
+    rospy.Subscriber("/rangeSonar1", Float32MultiArray, callback)
     rospy.spin()
+    rospy.loginfo('convert_range_sensors_values - EXIT!!!')
 
 
 if __name__ == '__main__':
