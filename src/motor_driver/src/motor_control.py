9edf3b2e12adcc6e3f771486ae907174f1102b24
denisshustov <denis.shustov.1984@gmail.com>
2020-10-23 08:27:47 -0400

    qqq

--- src/motor_driver/src/motor_control.py
+++ src/motor_driver/src/motor_control.py
@@ -11,43 +11,40 @@ import math
 from std_msgs.msg import Int32, Float64
 from threading import Thread
 
-class Motor_Driver(Thread):
+class Motor_Driver():
     CW = 1     # Clockwise Rotation
     CCW = 0    # Counterclockwise Rotation
 
     SPR = 200   # Steps per Revolution (360 / 1.8)
     delay_const = .00243
 
-    def __init__(self, dir, step, en, name, topic_name):
-        Thread.__init__(self)
+    def __init__(self, dir, step, en, name, topic_name, wheel_radius, rate, with_debug_info):
         self.delay = self.delay_const
         self.DIR = dir
         self.STEP = step
         self.EN = en
         self.prev_rpm = 0
         self.NAME  = name
-	self.TOPIC_NAME = topic_name
-	self.rpm = 0
+        self.TOPIC_NAME = topic_name
+        self.rpm = 0
+        self.WITH_DEBUG_INFO = with_debug_info
 
-        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.DIR, GPIO.OUT)
         GPIO.setup(self.STEP, GPIO.OUT)
 
         GPIO.setup(self.EN, GPIO.OUT)
         GPIO.output(self.EN, GPIO.HIGH)
-        rospy.loginfo("Disabled drivers, speed 0!")
+        if self.WITH_DEBUG_INFO:
+            rospy.loginfo("Disabled motor, speed 0! {}".format(self.TOPIC_NAME))
 
         self._last_received = rospy.get_time()
-        self._wheel_radius = rospy.get_param('~wheel_radius_meters', 0.73)
-        self._timeout = rospy.get_param('~timeout', 2)
-        self._rate = rospy.get_param('~rate', 1000)
+        self._wheel_radius = wheel_radius
+        self._rate = rate
 
-        GPIO.setmode(GPIO.BCM)
 
     def run(self):
-        rospy.loginfo("Thread " + self.NAME + " running")
         self.ros_sub_right = rospy.Subscriber(self.TOPIC_NAME , Float64, self.callback) #m/s
-	rate = rospy.Rate(self._rate)
+        rate = rospy.Rate(self._rate)
 
         while not rospy.is_shutdown():
             delay = rospy.get_time() - self._last_received
@@ -58,8 +55,9 @@ class Motor_Driver(Thread):
         if msg.data==0:
             self.rpm = 0
         else:
-            self.rpm = (self._wheel_radius * 0.10472) / msg.data
-            rospy.loginfo(self.NAME + " data [%f], RPM [%f]" % (msg.data, self.rpm))
+            self.rpm = msg.data / (self._wheel_radius * 0.10472)
+            if self.WITH_DEBUG_INFO:
+                rospy.loginfo(self.NAME + " data [%f], RPM [%f]" % (msg.data, self.rpm))
 
     def __rpm_to_delay(self, rpm):
         if rpm == 0:
@@ -78,13 +76,15 @@ class Motor_Driver(Thread):
 
         if rpm == 0:
             if self.prev_rpm != rpm:
-                rospy.loginfo("Disabled drivers, speed 0!")
+                if self.WITH_DEBUG_INFO:
+                    rospy.loginfo("Disabled motor, speed 0! {}".format(self.TOPIC_NAME))
                 GPIO.output(self.EN, GPIO.HIGH)
                 self.prev_rpm = rpm
             return
         else:
             if self.prev_rpm != rpm:
-                rospy.loginfo("Enabled drivers, rpm "+str(rpm))
+                if self.WITH_DEBUG_INFO:
+                    rospy.loginfo("Enabled drivers, rpm "+str(rpm))
                 GPIO.output(self.EN, GPIO.LOW)
 
         self.prev_rpm = rpm
@@ -110,19 +110,31 @@ class Motor_Driver(Thread):
 
 class Driver:
    # '''
-   # v = r x RPM x 0.10472 => v/RPM = r  x 0.10472 => RPM = (r x 0.10472) / v
+   # v = r x RPM x 0.10472 => RPM = v/(r x 0.10472) 
    # v: Linear velocity, in m/s
    # r: Radius, in meter
    # RPM: Angular velocity, in RPM (Rounds per Minute)
    # '''
 
     def __init__(self):
-        rospy.init_node('driver')
-        self.right_motor_driver = Motor_Driver(26, 19, 6,"right","/ppp/right_motor_control")
-        self.left_motor_driver = Motor_Driver(16, 20, 21,"left","/ppp/left_motor_control")
-        
-        self.right_motor_driver.start()
-        self.left_motor_driver.start()
+        GPIO.setmode(GPIO.BCM)
+        rospy.init_node('motor_control')
+
+        dir = rospy.get_param('~dir')
+        step = rospy.get_param('~step')
+        en = rospy.get_param('~en')
+        name = rospy.get_param('~name')
+        topic_name = rospy.get_param('~topic_name')
+        wheel_radius = rospy.get_param('~wheel_radius_meters', 0.073)
+        rate = rospy.get_param('~rate', 1000)
+        with_debug_info = rospy.get_param('~with_debug_info', False)
+
+        if dir == None or step == None or en == None or name == None or topic_name == None:
+            raise Exception('Wrong node params!!!')
+            return
+
+        self.motor_driver = Motor_Driver(dir, step, en, name, topic_name, wheel_radius, rate, with_debug_info)
+        self.motor_driver.run()
 
         rospy.loginfo("Initialization complete")
         
