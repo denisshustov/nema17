d75766d8825a9045ce8d1956e34821c5b32b6f25
denisshustov <denis.shustov.1984@gmail.com>
2020-11-13 17:46:14 +0200

    add test cmd vel

---
+++ src/test_cmd_vel/src/test_cmd_vel.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import rospy
+import time
+import sys
+from time import sleep
+import rospy
+from geometry_msgs.msg import Twist
+
+
+
+def main():   
+    rospy.loginfo('test_cmd_vel - start!')
+    rospy.init_node('test_cmd_vel', anonymous=True)
+    cmd_pub = rospy.Publisher('/ppp/cmd_vel', Twist, queue_size=10)
+
+    vel_msg = Twist()
+    vel_msg.linear.x = 0.2 # 0.2 m/s=> 20 sek/m
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z =0
+
+    rospy.loginfo('test_cmd_vel - publish val!')
+
+    now = rospy.Time.now()
+    rate = rospy.Rate(10)
+    while rospy.Time.now() < now + rospy.Duration.from_sec(20):
+            rospy.loginfo('.')
+            cmd_pub.publish(vel_msg)
+            rate.sleep() 
+            
+    vel_msg.linear.x = 0
+    cmd_pub.publish(vel_msg)
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
