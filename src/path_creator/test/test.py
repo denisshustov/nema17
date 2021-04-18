1720b1d381000a91780ae9cb24f2d4e5ce969b41
denisshustov <denis.shustov.1984@gmail.com>
2021-04-18 23:47:15 +0300

    hz

--- src/path_creator/test/test.py
+++ src/path_creator/test/test.py
@@ -34,7 +34,7 @@ img = cv2.imread(os.path.join(sys.path[0], 'img')+'/mymap_22.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 flag, image = cv2.threshold(gray, 205, 255, cv2.THRESH_BINARY)
 
-# image = img = load_array_to_file()
+image = img = load_array_to_file()
 cnt_inst = Conturs(image)
 cnts = cnt_inst.get_conturs(80)
 
@@ -52,7 +52,7 @@ cnts = cnt_inst.get_conturs(80)
 # xxx = cnt_inst.merge('11','14')
 # xxx = cnt_inst.merge('11','8')
 
-inter = cnt_inst.get_intersections(8)
+inter = cnt_inst.get_intersections(9)
 x=756
 y=299
 current_contur = cnts[0] #cnt_inst.get_contur_by_coord(x, y)
@@ -63,28 +63,28 @@ image = cv2.cvtColor(image,cv2.COLOR_GRAY2RGB)
 
 cnt_inst.show(image)
 
-i=1
-start_point = None
+# i=1
+# start_point = None
 
-while True:
-    if not current_contur.is_processed:
-        pth = PathFinder(current_contur.contur, image, 10, 1, start_point=start_point, debug_mode=True)
-        covered_points = pth.get_route()
-        if len(covered_points)>0:
-            start_point = covered_points[len(covered_points)-1]
-            current_contur.is_processed = True
-        else:
-            print('covered_points is empty, id={}!!!'.format(current_contur.id))
+# while True:
+#     if not current_contur.is_processed:
+#         pth = PathFinder(current_contur.contur, image, 10, 1, start_point=start_point, debug_mode=True)
+#         covered_points = pth.get_route()
+#         if len(covered_points)>0:
+#             start_point = covered_points[len(covered_points)-1]
+#             current_contur.is_processed = True
+#         else:
+#             print('covered_points is empty, id={}!!!'.format(current_contur.id))
 
-        # pth.show_mounting_point(img)
-        pth.show_path_point(image)
+#         # pth.show_mounting_point(img)
+#         pth.show_path_point(image)
 
-    current_conturs = cnt_inst.get_contur_in_order(current_contur)
-    if current_conturs == None or i>len(cnts):
-        break
-    current_contur = current_conturs[0]
+#     current_conturs = cnt_inst.get_contur_in_order(current_contur)
+#     if current_conturs == None or i>len(cnts):
+#         break
+#     current_contur = current_conturs[0]
 
-    i+=1
+#     i+=1
 
 # pth.show_grid(image)
 # pth.show_mounting_point(image)
